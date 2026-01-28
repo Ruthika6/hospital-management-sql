dff060598779f1d3b9a5f24359095cdb45fb2e35
K RUTHIKA REDDY <ruthikareddy09@gmail.com>
2026-01-28 17:05:37 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,44 @@
+CREATE DATABASE hospital_db;
+USE hospital_db;
+CREATE TABLE Patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT CHECK (age > 0),
+    gender VARCHAR(10),
+    city VARCHAR(50),
+    registration_date DATE
+);
+
+CREATE TABLE Doctors (
+    doctor_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    specialization VARCHAR(50),
+    experience_years INT
+);
+
+CREATE TABLE Appointments (
+    appointment_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    doctor_id INT,
+    appointment_date DATE,
+    status VARCHAR(20),
+    FOREIGN KEY (patient_id) REFERENCES Patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id)
+);
+
+CREATE TABLE Treatments (
+    treatment_id INT PRIMARY KEY AUTO_INCREMENT,
+    appointment_id INT,
+    diagnosis VARCHAR(100),
+    treatment_cost DECIMAL(10,2),
+    FOREIGN KEY (appointment_id) REFERENCES Appointments(appointment_id)
+);
+
+CREATE TABLE Bills (
+    bill_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    total_amount DECIMAL(10,2),
+    payment_status VARCHAR(20),
+    FOREIGN KEY (patient_id) REFERENCES Patients(patient_id)
+);
 
